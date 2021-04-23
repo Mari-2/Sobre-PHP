a0d873f9f3fd2caf449d2a3e2e89834af133b29e
Angeles <mariadelosasolorzanosanchez>
2021-04-23 04:23:36 -0400

    sobre php

---
+++ suma.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1><title>Ejemplo</title></h1>
+    </head>
+    <body>
+    <h1>Prueba de un mismo documento php y html.</h1>
+    <p>Probando php y html, junto al local host.</p>
+        <?php
+           $n1 = 201;
+           $n2 = "35";
+            
+           echo "Los numeros son ", $n1, " y ", $n2, ".<br />";
+           echo "Suma: ", $n1+$n2,"<br />";
+        ?>
+<ul>
+            <li>1</li>
+            <li> 2</li>
+            <li> 3</li>
+            <li> 4</li>
+         </ul>
+
+    </body>
+</html>
